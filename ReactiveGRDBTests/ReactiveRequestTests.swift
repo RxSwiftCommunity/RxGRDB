bcd539e31defab6ee1c816ed70be298dc9c6952a
Gwendal Roué <gr@pierlis.com>
2017-04-02 14:19:10 +0200

    Tests refactoring

--- ReactiveGRDBTests/ReactiveRequestTests.swift
+++ ReactiveGRDBTests/ReactiveRequestTests.swift
@@ -5,7 +5,7 @@ import RxSwift
 
 class TestDatabase<Writer: DatabaseWriter> {
     private let directoryURL: URL
-    let writer: Writer
+    private var writer: Writer!
     
     init(_ writer: (String) throws -> Writer) throws {
         directoryURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString, isDirectory: true)
@@ -13,7 +13,14 @@ class TestDatabase<Writer: DatabaseWriter> {
         self.writer = try writer(directoryURL.appendingPathComponent("db.sqlite").path)
     }
     
+    @discardableResult
+    func test(with testClosure: (Writer) throws -> ()) rethrows -> Self {
+        try testClosure(writer)
+        return self
+    }
+    
     deinit {
+        self.writer = nil
         try! FileManager.default.removeItem(at: directoryURL)
     }
 }
@@ -28,12 +35,16 @@ class ReactiveRequestTests: XCTestCase {
     override func tearDown() {
         disposeBag = nil
     }
+}
+
+extension ReactiveRequestTests {
+    func testSelectionInfoRxSelection() throws {
+        try TestDatabase({ try DatabaseQueue(path: $0) }).test(with: testSelectionInfoRxSelection)
+        try TestDatabase({ try DatabasePool(path: $0) }).test(with: testSelectionInfoRxSelection)
+    }
     
     func testSelectionInfoRxSelection(writer: DatabaseWriter) throws {
-        var selectionInfo1: SelectStatement.SelectionInfo! = nil
-        var selectionInfo2: SelectStatement.SelectionInfo! = nil
-        var selectionInfo3: SelectStatement.SelectionInfo! = nil
-        
+        var selectionInfos: [SelectStatement.SelectionInfo] = []
         try writer.write { db in
             try db.create(table: "table1") { t in
                 t.column("id", .integer).primaryKey()
@@ -46,87 +57,66 @@ class ReactiveRequestTests: XCTestCase {
                 t.column("b", .integer)
             }
             
-            selectionInfo1 = try db.makeSelectStatement("SELECT * FROM table1").selectionInfo
-            selectionInfo2 = try db.makeSelectStatement("SELECT id, a FROM table1").selectionInfo
-            selectionInfo3 = try db.makeSelectStatement("SELECT table1.id, table1.a, table2.a FROM table1 JOIN table2 ON table1.id = table2.id").selectionInfo
+            try selectionInfos.append(db.makeSelectStatement("SELECT * FROM table1").selectionInfo)
+            try selectionInfos.append(db.makeSelectStatement("SELECT id, a FROM table1").selectionInfo)
+            try selectionInfos.append(db.makeSelectStatement("SELECT table1.id, table1.a, table2.a FROM table1 JOIN table2 ON table1.id = table2.id").selectionInfo)
         }
         
-        var changed1 = false
-        var changed2 = false
-        var changed3 = false
-        
-        selectionInfo1.rx.selection(in: writer).subscribe(onNext: { _ in changed1 = true }).addDisposableTo(disposeBag)
-        selectionInfo2.rx.selection(in: writer).subscribe(onNext: { _ in changed2 = true }).addDisposableTo(disposeBag)
-        selectionInfo3.rx.selection(in: writer).subscribe(onNext: { _ in changed3 = true }).addDisposableTo(disposeBag)
+        var changes = selectionInfos.map { _ in false }
+        for (index, selectionInfo) in selectionInfos.enumerated() {
+            selectionInfo.rx
+                .selection(in: writer)
+                .subscribe(onNext: { _ in changes[index] = true })
+                .addDisposableTo(disposeBag)
+        }
         
         // Subscription immediately triggers an event
-        XCTAssertTrue(changed1)
-        XCTAssertTrue(changed2)
-        XCTAssertTrue(changed3)
+        XCTAssertEqual(changes, [true, true, true])
         
         try writer.write { db in
-            func reset() {
-                changed1 = false
-                changed2 = false
-                changed3 = false
-            }
+            func reset() { changes = changes.map { _ in false } }
             
             // Transaction triggers an event for concerned selection
             reset()
             try db.inTransaction {
                 try db.execute("INSERT INTO table1 (id, a, b) VALUES (NULL, 0, 0)")
-                XCTAssertFalse(changed1)
-                XCTAssertFalse(changed2)
-                XCTAssertFalse(changed3)
+                XCTAssertEqual(changes, [false, false, false])
                 return .commit
             }
-            XCTAssertTrue(changed1)
-            XCTAssertTrue(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [true, true, true])
             
             // Transaction triggers an event for concerned selection
             reset()
             try db.execute("INSERT INTO table2 (id, a, b) VALUES (NULL, 0, 0)")
-            XCTAssertFalse(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [false, false, true])
             
             // Transaction triggers an event for concerned selection
             reset()
             try db.execute("UPDATE table1 SET a = 1")
-            XCTAssertTrue(changed1)
-            XCTAssertTrue(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [true, true, true])
             
             // Transaction triggers an event for concerned selection
             reset()
             try db.execute("UPDATE table1 SET b = 1")
-            XCTAssertTrue(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertFalse(changed3)
+            XCTAssertEqual(changes, [true, false, false])
             
             // Transaction triggers an event for concerned selection
             reset()
             try db.execute("UPDATE table2 SET a = 1")
-            XCTAssertFalse(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [false, false, true])
             
             // Transaction triggers an event for concerned selection
             reset()
             try db.execute("UPDATE table2 SET b = 1")
-            XCTAssertFalse(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertFalse(changed3)
+            XCTAssertEqual(changes, [false, false, false])
         }
     }
-    
-    func testSelectionInfoRxSelection() throws {
-        let testQueue = try TestDatabase { try DatabaseQueue(path: $0) }
-        try testSelectionInfoRxSelection(writer: testQueue.writer)
-        
-        let testPool = try TestDatabase { try DatabasePool(path: $0) }
-        try testSelectionInfoRxSelection(writer: testPool.writer)
+}
+
+extension ReactiveRequestTests {
+    func testRequestRxSelection() throws {
+        try TestDatabase({ try DatabaseQueue(path: $0) }).test(with: testRequestRxSelection)
+        try TestDatabase({ try DatabasePool(path: $0) }).test(with: testRequestRxSelection)
     }
     
     func testRequestRxSelection(writer: DatabaseWriter) throws {
@@ -143,117 +133,69 @@ class ReactiveRequestTests: XCTestCase {
             }
         }
         
-        var changed1 = false
-        var changed2 = false
-        var changed3 = false
+        let requests = [
+            SQLRequest("SELECT * FROM table1"),
+            SQLRequest("SELECT id, a FROM table1"),
+            SQLRequest("SELECT table1.id, table1.a, table2.a FROM table1 JOIN table2 ON table1.id = table2.id")]
         
-        let request1 = SQLRequest("SELECT * FROM table1")
-        let request2 = SQLRequest("SELECT id, a FROM table1")
-        let request3 = SQLRequest("SELECT table1.id, table1.a, table2.a FROM table1 JOIN table2 ON table1.id = table2.id")
-        
-        request1.rx.selection(in: writer).subscribe(onNext: { _ in changed1 = true }).addDisposableTo(disposeBag)
-        request2.rx.selection(in: writer).subscribe(onNext: { _ in changed2 = true }).addDisposableTo(disposeBag)
-        request3.rx.selection(in: writer).subscribe(onNext: { _ in changed3 = true }).addDisposableTo(disposeBag)
+        var changes = requests.map { _ in false }
+        for (index, request) in requests.enumerated() {
+            request.rx
+                .selection(in: writer)
+                .subscribe(onNext: { _ in changes[index] = true })
+                .addDisposableTo(disposeBag)
+        }
         
         // Subscription immediately triggers an event
-        XCTAssertTrue(changed1)
-        XCTAssertTrue(changed2)
-        XCTAssertTrue(changed3)
+        XCTAssertEqual(changes, [true, true, true])
         
         try writer.write { db in
-            func reset() {
-                changed1 = false
-                changed2 = false
-                changed3 = false
-            }
-            
+            func reset() { changes = changes.map { _ in false } }
             
             // Transaction triggers an event for concerned requests
             reset()
             try db.inTransaction {
                 try db.execute("INSERT INTO table1 (id, a, b) VALUES (NULL, 0, 0)")
-                XCTAssertFalse(changed1)
-                XCTAssertFalse(changed2)
-                XCTAssertFalse(changed3)
+                XCTAssertEqual(changes, [false, false, false])
                 return .commit
             }
-            XCTAssertTrue(changed1)
-            XCTAssertTrue(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [true, true, true])
             
             // Transaction triggers an event for concerned requests
             reset()
             try db.execute("INSERT INTO table2 (id, a, b) VALUES (NULL, 0, 0)")
-            XCTAssertFalse(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [false, false, true])
             
             // Transaction triggers an event for concerned requests
             reset()
             try db.execute("UPDATE table1 SET a = 1")
-            XCTAssertTrue(changed1)
-            XCTAssertTrue(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [true, true, true])
             
             // Transaction triggers an event for concerned requests
             reset()
             try db.execute("UPDATE table1 SET b = 1")
-            XCTAssertTrue(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertFalse(changed3)
+            XCTAssertEqual(changes, [true, false, false])
             
             // Transaction triggers an event for concerned requests
             reset()
             try db.execute("UPDATE table2 SET a = 1")
-            XCTAssertFalse(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertTrue(changed3)
+            XCTAssertEqual(changes, [false, false, true])
             
             // Transaction triggers an event for concerned requests
             reset()
             try db.execute("UPDATE table2 SET b = 1")
-            XCTAssertFalse(changed1)
-            XCTAssertFalse(changed2)
-            XCTAssertFalse(changed3)
+            XCTAssertEqual(changes, [false, false, false])
         }
     }
-    
-    func testRequestRxSelection() throws {
-        let testQueue = try TestDatabase { try DatabaseQueue(path: $0) }
-        try testRequestRxSelection(writer: testQueue.writer)
-        
-        let testPool = try TestDatabase { try DatabasePool(path: $0) }
-        try testRequestRxSelection(writer: testPool.writer)
+}
+
+extension ReactiveRequestTests {
+    func testRequestRxFetchAll() throws {
+        try TestDatabase({ try DatabaseQueue(path: $0) }).test(with: testRequestRxFetchAll)
+        try TestDatabase({ try DatabasePool(path: $0) }).test(with: testRequestRxFetchAll)
     }
-    
+
     func testRequestRxFetchAll(writer: DatabaseWriter) throws {
-        class Person: Record {
-            var id: Int64?
-            var name: String
-            
-            init(id: Int64?, name: String) {
-                self.id = id
-                self.name = name
-                super.init()
-            }
-            
-            required init(row: Row) {
-                id = row.value(named: "id")
-                name = row.value(named: "name")
-                super.init(row: row)
-            }
-            
-            override class var databaseTableName: String { return "persons" }
-            
-            override var persistentDictionary: [String : DatabaseValueConvertible?] {
-                return ["id": id, "name": name]
-            }
-            
-            override func didInsert(with rowID: Int64, for column: String?) {
-                id = rowID
-            }
-        }
-        
         try writer.write { db in
             try db.create(table: "persons") { t in
                 t.column("id", .integer).primaryKey()
@@ -263,21 +205,20 @@ class ReactiveRequestTests: XCTestCase {
             try Person(id: nil, name: "Barbara").insert(db)
         }
         
-        let expectations = (UInt(2)...3).map { i -> XCTestExpectation in
-            let expectation = self.expectation(description: "")
-            expectation.expectedFulfillmentCount = i
-            expectation.assertForOverFulfill = false
-            return expectation
-        }
+        // Expectation for later transaction
+        let expectation = self.expectation(description: "1")
+        expectation.expectedFulfillmentCount = 2    // two because subscription receives an immediate event, then a second on transaction.
         
+        // Subscribe tp a request
         let request = Person.order(Column("name"))
         var records: [Person] = []
-        request.rx.fetchAll(in: writer)
+        request.rx
+            .fetchAll(in: writer)
             .subscribe(onNext: {
-                // events happen on the main thread
+                // events are expected to be delivered on the main thread
                 XCTAssertTrue(Thread.isMainThread)
                 records = $0
-                expectations.forEach { $0.fulfill() }
+                expectation.fulfill()
             })
             .addDisposableTo(disposeBag)
         
@@ -285,21 +226,37 @@ class ReactiveRequestTests: XCTestCase {
         XCTAssertEqual(records.map({ $0.name }), ["Arthur", "Barbara"])
         
         try writer.write { db in
+            // Transaction triggers an asynchronous event
             try Person(id: nil, name: "Craig").insert(db)
-            wait(for: [expectations[0]], timeout: 1)
+            waitForExpectations(timeout: 1, handler: nil)
             XCTAssertEqual(records.map({ $0.name }), ["Arthur", "Barbara", "Craig"])
-            
-            try Person.deleteAll(db)
-            wait(for: [expectations[1]], timeout: 1)
-            XCTAssertEqual(records.map({ $0.name }), [])
         }
     }
     
-    func testRequestRxFetchAll() throws {
-        let testQueue = try TestDatabase { try DatabaseQueue(path: $0) }
-        try testRequestRxFetchAll(writer: testQueue.writer)
+    class Person: Record {
+        var id: Int64?
+        var name: String
         
-        let testPool = try TestDatabase { try DatabasePool(path: $0) }
-        try testRequestRxFetchAll(writer: testPool.writer)
+        init(id: Int64?, name: String) {
+            self.id = id
+            self.name = name
+            super.init()
+        }
+        
+        required init(row: Row) {
+            id = row.value(named: "id")
+            name = row.value(named: "name")
+            super.init(row: row)
+        }
+        
+        override class var databaseTableName: String { return "persons" }
+        
+        override var persistentDictionary: [String : DatabaseValueConvertible?] {
+            return ["id": id, "name": name]
+        }
+        
+        override func didInsert(with rowID: Int64, for column: String?) {
+            id = rowID
+        }
     }
 }
