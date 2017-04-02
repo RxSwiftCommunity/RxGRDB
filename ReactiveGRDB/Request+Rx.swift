fdb41c89f60a148b2eb98f30b5436e3333741f62
Gwendal Roué <gr@pierlis.com>
2017-04-02 16:11:17 +0200

    Diff WIP

--- ReactiveGRDB/Request+Rx.swift
+++ ReactiveGRDB/Request+Rx.swift
@@ -45,3 +45,117 @@ final class RequestChangesObservable<R: Request> : ObservableType {
         return selectionInfo.rx.changes(in: writer).subscribe(observer)
     }
 }
+
+final class RequestFetchObservable<R: Request, ResultType> : ObservableType {
+    typealias E = ResultType
+    
+    let writer: DatabaseWriter
+    let request: R
+    let resultQueue: DispatchQueue?
+    let fetch: (Database) throws -> ResultType
+    
+    init(writer: DatabaseWriter, request: R, fetch: @escaping (Database) throws -> ResultType, resultQueue: DispatchQueue? = nil) {
+        self.writer = writer
+        self.request = request
+        self.fetch = fetch
+        self.resultQueue = resultQueue
+    }
+    
+    func subscribe<O>(_ observer: O) -> Disposable where O : ObserverType, O.E == E {
+        let orderingQueue = DispatchQueue(label: "ReactiveGRDB.results")
+        
+        var initial = true
+        let disposable = AnyRequest(request).rx.changes(in: writer).subscribe { event in
+            switch event {
+            case .next(let db):
+                if initial {
+                    // Emit immediately on subscription
+                    initial = false
+                    do {
+                        try observer.onNext(self.fetch(db))
+                    } catch {
+                        observer.onError(error)
+                    }
+                } else {
+                    let semaphore = DispatchSemaphore(value: 0)
+                    var result: Result<E>? = nil
+                    do {
+                        try self.writer.readFromCurrentState { db in
+                            result = Result.wrap { try self.fetch(db) }
+                            semaphore.signal()
+                        }
+                    } catch {
+                        result = .failure(error)
+                        semaphore.signal()
+                    }
+                    
+                    orderingQueue.async {
+                        _ = semaphore.wait(timeout: .distantFuture)
+                        
+                        func notify() {
+                            switch result! {
+                            case .success(let results):
+                                observer.onNext(results)
+                            case .failure(let error):
+                                observer.onError(error)
+                            }
+                        }
+                        
+                        if let resultQueue = self.resultQueue {
+                            resultQueue.async(execute: notify)
+                        } else {
+                            notify()
+                        }
+                    }
+                }
+            case .error(let error):
+                observer.onError(error)
+            case .completed:
+                observer.onCompleted()
+            }
+        }
+        
+        return disposable
+    }
+}
+
+private enum Result<Value> {
+    case success(Value)
+    case failure(Error)
+    
+    static func wrap(_ value: () throws -> Value) -> Result<Value> {
+        do {
+            return try .success(value())
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    /// Evaluates the given closure when this `Result` is a success, passing the
+    /// unwrapped value as a parameter.
+    ///
+    /// Use the `map` method with a closure that does not throw. For example:
+    ///
+    ///     let possibleData: Result<Data> = .success(Data())
+    ///     let possibleInt = possibleData.map { $0.count }
+    ///     try print(possibleInt.unwrap())
+    ///     // Prints "0"
+    ///
+    ///     let noData: Result<Data> = .failure(error)
+    ///     let noInt = noData.map { $0.count }
+    ///     try print(noInt.unwrap())
+    ///     // Throws error
+    ///
+    /// - parameter transform: A closure that takes the success value of
+    ///   the instance.
+    /// - returns: A `Result` containing the result of the given closure. If
+    ///   this instance is a failure, returns the same failure.
+    func map<T>(_ transform: (Value) -> T) -> Result<T> {
+        switch self {
+        case .success(let value):
+            return .success(transform(value))
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+}
--- ReactiveGRDB/TypedRequest+Rx.swift
+++ ReactiveGRDB/TypedRequest+Rx.swift
@@ -1,3 +1,4 @@
+import Foundation
 import GRDB
 import RxSwift
 
@@ -31,86 +32,55 @@ extension Reactive where Base: TypedRequest, Base.Fetched: RowConvertible {
             fetch: { try self.base.fetchOne($0) },
             resultQueue: resultQueue).asObservable()
     }
-}
-
-final class RequestFetchObservable<R: Request, ResultType> : ObservableType {
-    typealias E = ResultType
     
-    let writer: DatabaseWriter
-    let request: R
-    let resultQueue: DispatchQueue?
-    let fetch: (Database) throws -> ResultType
-    
-    init(writer: DatabaseWriter, request: R, fetch: @escaping (Database) throws -> ResultType, resultQueue: DispatchQueue? = nil) {
-        self.writer = writer
-        self.request = request
-        self.fetch = fetch
-        self.resultQueue = resultQueue
-    }
-    
-    func subscribe<O>(_ observer: O) -> Disposable where O : ObserverType, O.E == E {
-        let orderingQueue = DispatchQueue(label: "ReactiveGRDB.results")
-        
-        var initial = true
-        let disposable = AnyRequest(request).rx.changes(in: writer).subscribe { event in
-            switch event {
-            case .next(let db):
-                if initial {
-                    initial = false
-                    do {
-                        try observer.onNext(self.fetch(db))
-                    } catch {
-                        observer.onError(error)
-                    }
-                } else {
-                    let semaphore = DispatchSemaphore(value: 0)
-                    var result: Result<E>? = nil
-                    do {
-                        try self.writer.readFromCurrentState { db in
-                            result = Result.wrap { try self.fetch(db) }
-                            semaphore.signal()
-                        }
-                    } catch {
-                        result = .failure(error)
-                        semaphore.signal()
-                    }
-                    
-                    orderingQueue.async {
-                        _ = semaphore.wait(timeout: .distantFuture)
-                        
-                        func notify() {
-                            switch result! {
-                            case .success(let results):
-                                observer.onNext(results)
-                            case .failure(let error):
-                                observer.onError(error)
-                            }
-                        }
-                        
-                        if let resultQueue = self.resultQueue {
-                            resultQueue.async(execute: notify)
-                        } else {
-                            notify()
-                        }
-                    }
-                }
-            case .error(let error):
-                observer.onError(error)
-            case .completed:
-                observer.onCompleted()
-            }
-        }
-        
-        return disposable
+    public func diff(in writer: DatabaseWriter, resultQueue: DispatchQueue = DispatchQueue.main) -> Observable<(RequestResults<Base.Fetched>, RequestDiff)> {
+        let diffQueue = DispatchQueue(label: "ReactiveGRDB.diff")
+        let items = base.bound(to: Item<Base.Fetched>.self).rx.fetchAll(in: writer, resultQueue: diffQueue)
+        return Diff(items: items.asObservable(), resultQueue: resultQueue).asObservable()
     }
 }
 
-public final class Item<T: RowConvertible> : RowConvertible, Equatable {
+public struct RequestResults<Fetched: RowConvertible> {
+    fileprivate let items: [Item<Fetched>]
+    
+    fileprivate init(items: [Item<Fetched>]) {
+        self.items = items
+    }
+    
+    public subscript(_ indexPath: IndexPath) -> Fetched {
+        return items[indexPath[1]].record
+    }
+}
+
+public enum RequestDiff {
+    case snapshot
+    case changes([Change])
+    
+    public enum Change {
+        /// An insertion event, at given indexPath.
+        case insertion(indexPath: IndexPath)
+        
+        /// A deletion event, at given indexPath.
+        case deletion(indexPath: IndexPath)
+        
+        /// A move event, from indexPath to newIndexPath. The *changes* are a
+        /// dictionary whose keys are column names, and values the old values that
+        /// have been changed.
+        case move(indexPath: IndexPath, newIndexPath: IndexPath, changes: [String: DatabaseValue])
+        
+        /// An update event, at given indexPath. The *changes* are a dictionary
+        /// whose keys are column names, and values the old values that have
+        /// been changed.
+        case update(indexPath: IndexPath, changes: [String: DatabaseValue])
+    }
+}
+
+final class Item<Fetched: RowConvertible> : RowConvertible, Equatable {
     let row: Row
     
     // Records are lazily loaded
-    lazy var record: T = {
-        var record = T(row: self.row)
+    lazy var record: Fetched = {
+        var record = Fetched(row: self.row)
         record.awakeFromFetch(row: self.row)
         return record
     }()
@@ -124,44 +94,44 @@ public final class Item<T: RowConvertible> : RowConvertible, Equatable {
     }
 }
 
-
-private enum Result<Value> {
-    case success(Value)
-    case failure(Error)
+final class Diff<Fetched> : ObservableType where Fetched: RowConvertible {
+    typealias E = (RequestResults<Fetched>, RequestDiff)
     
-    static func wrap(_ value: () throws -> Value) -> Result<Value> {
-        do {
-            return try .success(value())
-        } catch {
-            return .failure(error)
-        }
+    let items: Observable<[Item<Fetched>]>
+    let resultQueue: DispatchQueue
+    
+    init(items: Observable<[Item<Fetched>]>, resultQueue: DispatchQueue) {
+        self.items = items
+        self.resultQueue = resultQueue
     }
     
-    /// Evaluates the given closure when this `Result` is a success, passing the
-    /// unwrapped value as a parameter.
-    ///
-    /// Use the `map` method with a closure that does not throw. For example:
-    ///
-    ///     let possibleData: Result<Data> = .success(Data())
-    ///     let possibleInt = possibleData.map { $0.count }
-    ///     try print(possibleInt.unwrap())
-    ///     // Prints "0"
-    ///
-    ///     let noData: Result<Data> = .failure(error)
-    ///     let noInt = noData.map { $0.count }
-    ///     try print(noInt.unwrap())
-    ///     // Throws error
-    ///
-    /// - parameter transform: A closure that takes the success value of
-    ///   the instance.
-    /// - returns: A `Result` containing the result of the given closure. If
-    ///   this instance is a failure, returns the same failure.
-    func map<T>(_ transform: (Value) -> T) -> Result<T> {
-        switch self {
-        case .success(let value):
-            return .success(transform(value))
-        case .failure(let error):
-            return .failure(error)
+    func subscribe<O>(_ observer: O) -> Disposable where O : ObserverType, O.E == E {
+        var lastItems: [Item<Fetched>]? = nil
+        
+        return items.subscribe { event in
+            switch event {
+            case .next(let new):
+                if let last = lastItems {
+                    // TODO: compute changes from last to items
+                    let pair: E = (RequestResults(items: new), .changes([]))
+                    lastItems = new
+                    self.resultQueue.async {
+                        observer.onNext(pair)
+                    }
+                } else {
+                    // Emit immediately on subscription
+                    lastItems = new
+                    observer.onNext((RequestResults(items: new), .snapshot))
+                }
+            case .error(let error):
+                self.resultQueue.async {
+                    observer.onError(error)
+                }
+            case .completed:
+                self.resultQueue.async {
+                    observer.onCompleted()
+                }
+            }
         }
     }
 }
