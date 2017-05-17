23dbfe41d1559717dce9609c24f7d2d0abb8ff29
Gwendal Roué <gr@pierlis.com>
2017-05-17 19:21:38 +0200

    Some CocoaPods apps for testing

---
+++ Tests/CocoaPods/RxGRDB+GRDBCipher/RxGRDB/ViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+import GRDBCipher
+import RxSwift
+import RxGRDB
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var maxScoreLabel: UILabel!
+//    let maxScore = SQLRequest("SELECT max(score) FROM persons").bound(to: Int.self)
+    let maxScore = Person.select(max(Column("score"))).bound(to: Int.self)
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        maxScore.rx
+            .fetchOne(in: dbQueue)
+            .subscribe(onNext: { [weak self] score in self?.maxScoreDidChange(score) })
+            .addDisposableTo(disposeBag)
+    }
+    
+    @IBAction func reset() {
+        try! dbQueue.inDatabase { db in
+            _ = try Person.deleteAll(db)
+        }
+    }
+    
+    @IBAction func addPerson() {
+        try! dbQueue.inDatabase { db in
+            try Person(name: Person.randomName(), score: Person.randomScore()).insert(db)
+        }
+    }
+    
+    private func maxScoreDidChange(_ score: Int?) {
+        if let score = score {
+            maxScoreLabel.text = String(describing: score)
+        } else {
+            maxScoreLabel.text = "None"
+        }
+    }
+}
