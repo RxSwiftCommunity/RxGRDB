b4bf8438c2e8bab90c7ea49acba4b09b7c74d900
Gwendal Roué <gr@pierlis.com>
2017-05-22 20:50:34 +0200

    Update test apps for v0.3.0

--- Tests/CocoaPods/RxGRDB/RxGRDB/ViewController.swift
+++ Tests/CocoaPods/RxGRDB/RxGRDB/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var maxScoreLabel: UILabel!
 //    let maxScore = SQLRequest("SELECT max(score) FROM persons").bound(to: Int.self)
-    let maxScore = Person.select(max(Column("score"))).bound(to: Int.self)
+    let maxScore = Person.select(max(Column("score"))).asRequest(of: Int.self)
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
