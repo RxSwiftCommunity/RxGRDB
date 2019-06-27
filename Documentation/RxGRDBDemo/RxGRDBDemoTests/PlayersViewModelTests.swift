a3602de7357b48fd588839f2c2ca7057573e2cc5
Gwendal Roué <gr@pierlis.com>
2019-06-27 08:52:45 +0200

    PlayersViewModelTests

---
+++ Documentation/RxGRDBDemo/RxGRDBDemoTests/PlayersViewModelTests.swift
@@ -0,0 +1,29 @@
+import GRDB
+import RxBlocking
+import RxSwift
+import XCTest
+
+class PlayersViewModelTests: XCTestCase {
+    override func setUp() {
+        let dbQueue = DatabaseQueue()
+        try! AppDatabase().setup(dbQueue)
+        Current = World(database: { dbQueue })
+    }
+    
+    func testInitialStateFromEmptyDatabase() throws {
+        let viewModel = PlayersViewModel()
+        let orderingButtonTitle = try viewModel.orderingButtonTitle.take(1).toBlocking(timeout: 1).single()
+        let players = try viewModel.players.take(1).toBlocking(timeout: 1).single()
+        XCTAssertNil(orderingButtonTitle)
+        XCTAssert(players.isEmpty)
+    }
+    
+    func testInitialStateFromNonEmptyDatabase() throws {
+        try Current.players().populateIfEmpty()
+        let viewModel = PlayersViewModel()
+        let orderingButtonTitle = try viewModel.orderingButtonTitle.take(1).toBlocking(timeout: 1).single()
+        let players = try viewModel.players.take(1).toBlocking(timeout: 1).single()
+        XCTAssertEqual(orderingButtonTitle, "Score ⬇︎")
+        XCTAssert(!players.isEmpty)
+    }
+}
